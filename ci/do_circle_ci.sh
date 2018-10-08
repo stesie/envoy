a8d7a380d0ba4e030224d99cba41427c00568cbc
Lizan Zhou <lizan@tetrate.io>
2018-10-08 08:29:22 +0000

    Test

--- ci/do_circle_ci.sh
+++ ci/do_circle_ci.sh
@@ -13,7 +13,7 @@ export ENVOY_SRCDIR="$(pwd)"
 # xlarge resource_class.
 # See note: https://circleci.com/docs/2.0/configuration-reference/#resource_class for why we
 # hard code this (basically due to how docker works).
-export NUM_CPUS=8
+export NUM_CPUS=1
 
 # CircleCI doesn't support IPv6 by default, so we run all tests with IPv4, and
 # a limited subset with IPv6 using "machine: true" and do_circle_ci_ipv6_tests.sh
