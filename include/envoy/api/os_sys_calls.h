8f213674595047851ff96e773f37fc3ddd4c1c1a
Genevieve LEsperance <glesperance@pivotal.io>
2019-04-23 14:27:26 -0400

    WIP
    Fix //test/server:options_impl_test
    
    Signed-off-by: Yael Harel <yharel@pivotal.io>
    
    Signed-off-by: Genevieve LEsperance <glesperance@pivotal.io>

--- include/envoy/api/os_sys_calls.h
+++ include/envoy/api/os_sys_calls.h
@@ -175,6 +175,9 @@ public:
    * @see man 2 accept
    */
   virtual SysCallSocketResult accept(SOCKET_FD sockfd, sockaddr* addr, socklen_t* addr_len) PURE;
+
+
+  virtual SysCallIntResult sched_getaffinity(pid_t pid, size_t cpusetsize, cpu_set_t* mask) PURE;
 };
 
 using OsSysCallsPtr = std::unique_ptr<OsSysCalls>;
