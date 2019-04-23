9495432ae04d48954c0b4d600469c186f2205793
Yael Harel <yharel@pivotal.io>
2019-04-23 14:44:56 -0400

    All the test are passing on Linux
    Fixed //test/extensions/filters/network/mysql_proxy:mysql_integration_test
    
    Signed-off-by: Genevieve LEsperance <glesperance@pivotal.io>
    
    Signed-off-by: Yael Harel <yharel@pivotal.io>

--- test/extensions/filters/network/mysql_proxy/mysql_integration_test.cc
+++ test/extensions/filters/network/mysql_proxy/mysql_integration_test.cc
@@ -1,3 +1,5 @@
+#include <pthread.h>
+
 #include "extensions/filters/network/mysql_proxy/mysql_codec.h"
 #include "extensions/filters/network/mysql_proxy/mysql_codec_clogin.h"
 #include "extensions/filters/network/mysql_proxy/mysql_codec_clogin_resp.h"
@@ -10,7 +12,6 @@
 #include "test/mocks/network/mocks.h"
 #include "test/test_common/network_utility.h"
 
-#include "fmt/printf.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "mysql_test_utils.h"
